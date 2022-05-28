99663fb41651927ccd2e949dfee4afc524a74eef
Serkan Parlak <@GTUSR0306>
2022-05-29 02:46:12 +0300

    edit server js

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ app.get('/*', (req, res) =>
 
 // Start the app by listening on the default Heroku port
 // app.listen(process.env.PORT || 8080);
-app.listen(8080);
+app.listen(process.env.PORT || 80, () => {
+  console.log("Listening on port 80");
+});
